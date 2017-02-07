eef530528966e082564dc82c91b7b9e6db3cddc6
James Talor <james@jtwd.co.uk>
2017-02-07 22:08:59 +0000

    Step 2

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+
+import webpackConfig from '../webpack.config.dev';
+
+
 const PORT = 8115;
 
 let app = express();
+
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
