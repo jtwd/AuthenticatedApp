8e56c4e0e5af903a2ba3e4deb502de4e0d98f2d1
jtwd <james@jtwd.co.uk>
2017-02-08 14:53:27 +0000

    Check for uniqueness of username and email on the server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,29 +1,55 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+import isEmpty from 'lodash/isEmpty';
 
 import User from '../models/user';
 
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 
 const router = express.Router();
 
 
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data);
+
+  return User.query({
+    where: {email: data.email},
+    orWhere: {username: data.username }
+  }).fetch().then(user => {
+    if(user) {
+      if(user.get('username') === data.username) {
+        errors.username = 'Username already exists'
+      }
+      if(user.get('email') === data.email) {
+        errors.email = 'Email already exists'
+      }
+    }
+
+    return {
+      errors,
+      isValid: isEmpty(errors)
+    };
+  });
+
+}
+
 router.post('/', (req, res) => {
-  const { errors, isValid } = validateInput(req.body);
+  validateInput(req.body, commonValidations).then(({ errors, isValid}) => {
+    if(isValid) {
+      const { username, password, timezone, email} = req.body;
+      const password_digest = bcrypt.hashSync(password, 10);
 
-  if(isValid) {
-    const { username, password, timezone, email} = req.body;
-    const password_digest = bcrypt.hashSync(password, 10);
+      User.forge({
+        username, timezone, email, password_digest
+      }, { hasTimestamps: true }).save()
+        .then(user => res.json({ success: true }))
+        .catch(err => res.status(500).json({ error: err}));
 
-    User.forge({
-      username, timezone, email, password_digest
-    }, { hasTimestamps: true }).save()
-      .then(user => res.json({ success: true }))
-      .catch(err => res.status(500).json({ error: err}));
+    } else {
+      res.status(400).json(errors);
+    }
+  });
 
-  } else {
-    res.status(400).json(errors);
-  }
 });
 
 
